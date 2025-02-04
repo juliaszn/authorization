f1908ce32387021b768c3bde4ac96c35936cdc6c
juliaszn <yuliaaaas04@gmail.com>
2025-02-04 21:14:51 +0400

    Added html form in login.php

--- login.php
+++ login.php
@@ -1,1 +1,27 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>Welcome Back</h1>
+
+<form method="post">
+    <label for="email"> Email:
+        <input type ="email" name="email" required>
+    </label>
+    <br><br>
+    <label for="password"> Password:
+        <input type ="password" name="password" required>
+    </label>
+    <br><br>
+    <input type="submit" value="Login"
+</form>
+
+</body>
+</html>
